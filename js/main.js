11c10c56413913384d1cef02c2138059c1e29d13
Kaushal Patel <kaushalpatel922@gmail.com>
2016-06-16 17:33:56 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 
+$(document).ready(function(){
+  $("body").css("-webkit-user-select","none");
+  $("body").css("-moz-user-select","none");
+  $("body").css("-ms-user-select","none");
+  $("body").css("-o-user-select","none");
+  $("body").css("user-select","none");
+});
 
 $(document).ready(function() {
+  // defining variables
   var current_number = "";
   var exponent = "";
   var answer;
@@ -9,14 +17,18 @@ $(document).ready(function() {
 
   $('.num').on('click', function () {
     updateNum(this.id);
-  })
+  });
 
+  $('.operator').on('click', function() {
+    updateExp(this.id);
+  });
 
   function updateNum(num) {
     if (num == '.') {
       if(current_number.indexOf('.') !== -1) {
-
+        return false;
       } else {
+        // update current_number to current_number + number;
         current_number +=num;
       }
     } else if (num == 'negative') {
@@ -33,6 +45,19 @@ $(document).ready(function() {
 
   function displayNum() {
     $('#display').val(exponent + current_number);
+  }
+
+  function updateExponent(operator) {
+    exponent = exponent + current_number + operator;
+    current_number='';
+    displayNum();
+  }
+
+  function evaluateExp() {
+    updateExponent('');
+
+    var test = parseFloat(exp.substr(exp.length-1));
+
   }
 
 })
